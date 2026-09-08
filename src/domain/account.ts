420c9f7f3f64b6f9b1905adfe903129c850443cd
Tito <github@mynameistito.com>
2026-09-08 12:17:42 +1200

    refactor: reuse canonical timestamp schema for accounts

--- src/domain/account.ts
+++ src/domain/account.ts
@@ -3,15 +3,10 @@ import { Schema } from "effect";
 import {
   AccountIdSchema,
   ConnectionIdSchema,
+  IsoDateTimeSchema,
   ProviderAccountIdSchema,
   ProviderIdSchema,
 } from "@/domain/identifiers";
-
-const IsoDateTimeSchema = Schema.String.pipe(
-  Schema.check(
-    Schema.makeFilter((value: string) => !Number.isNaN(Date.parse(value)))
-  )
-);
 
 const ProviderAccountFields = {
   availableBalance: Schema.NullOr(Schema.Number),
